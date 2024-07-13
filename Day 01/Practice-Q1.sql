3e8e4e7b04f0cf168f2ae0bc781dcfdd82dc12cd
dev-kant-kumar <dev.mytechhandle@gmail.com>
2024-07-13 23:10:33 +0530

    Practice Q1 completed

--- Day 01/Practice-Q1.sql
+++ Day 01/Practice-Q1.sql
@@ -1,8 +1,9 @@
 -- create a database for your company xyz.
 -- Create a table inside this DB to store employee info (id,name,salary)
 
-CREATE DATABASE XYZ;
-USE XYZ;
+CREATE DATABASE IF NOT EXISTS XYZ;
+USE XYZ;                     -- command to use XYZ database
+DROP DATABASE IF EXISTS XYZ; -- command to drop database XYZ
 
 -- creating employee table in XYZ database
 CREATE TABLE employee (Id INT PRIMARY KEY, Name VARCHAR (20) NOT NULL,Salary INT NOT NULL);
@@ -17,6 +18,9 @@ INSERT INTO employee (Id,Name,Salary) VALUES
 (1,"Adam",25000),
 (2,"Bob",30000),
 (3, "Casey",40000);
+
+-- command to drop employee table
+DROP TABLE employee ;
 
 -- selecting & viewing all employee table data
 SELECT * FROM employee;
