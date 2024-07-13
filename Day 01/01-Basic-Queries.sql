36cff8d89bb8cd88f08a26b8a046561f399396d7
dev-kant-kumar <dev.mytechhandle@gmail.com>
2024-07-13 14:06:54 +0530

    Updating Day 01 learning

---
+++ Day 01/01-Basic-Queries.sql
@@ -0,0 +1,9 @@
+show databases;
+use college;
+show tables;
+select * from student;
+select distinct * from student;
+select * from student where RegistrationNo in (1001,1005,1007);
+desc student;
+show columns from student;
+SHOW CREATE TABLE student;
